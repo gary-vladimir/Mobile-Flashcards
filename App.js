72f9026e74837ed84a41242f9ac7e934c1487313
gary-vladimir <gary@garybricks.com>
2021-07-09 22:03:55 -0500

    added bottom tab navigation with correct icons

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text>Welcome Gary!</Text>
-            <StatusBar style="auto" />
+        <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+            <Text>Home!</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+function SettingsScreen() {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        if (route.name === 'Home') {
+                            return (
+                                <Ionicons
+                                    name={focused ? 'home' : 'home-outline'}
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        } else if (route.name === 'Settings') {
+                            return (
+                                <Ionicons
+                                    name={
+                                        focused
+                                            ? 'add-circle'
+                                            : 'add-circle-outline'
+                                    }
+                                    size={size}
+                                    color={color}
+                                />
+                            );
+                        }
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'gray',
+                }}
+            >
+                <Tab.Screen name="Home" component={HomeScreen} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
