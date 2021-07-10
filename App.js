1ae8c36bee5e596c2f6adebeefabf916f0d821a8
gary-vladimir <gary@garybricks.com>
2021-07-09 23:10:47 -0500

    added custom status bar

--- App.js
+++ App.js
@@ -1,50 +1,69 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './components/Home';
 import NewDeck from './components/NewDeck';
+import Constants from 'expo-constants';
 
 const Tab = createBottomTabNavigator();
 
-export default function App() {
+function CustomStatusBar({ backgroundColor, ...props }) {
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        if (route.name === 'Home') {
-                            return (
-                                <Ionicons
-                                    name={focused ? 'home' : 'home-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        } else if (route.name === 'Add Deck') {
-                            return (
-                                <Ionicons
-                                    name={
-                                        focused
-                                            ? 'add-circle'
-                                            : 'add-circle-outline'
-                                    }
-                                    size={size}
-                                    color={color}
-                                />
-                            );
-                        }
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: '#E63946',
-                    inactiveTintColor: '#1D3557',
-                }}
-            >
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Add Deck" component={NewDeck} />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+            <StatusBar
+                translucent
+                backgroundColor={backgroundColor}
+                {...props}
+            />
+        </View>
     );
+}
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <NavigationContainer>
+                <CustomStatusBar
+                    backgroundColor={'#457B9D'}
+                    barStyle="light-content"
+                />
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            if (route.name === 'Home') {
+                                return (
+                                    <Ionicons
+                                        name={focused ? 'home' : 'home-outline'}
+                                        size={size}
+                                        color={color}
+                                    />
+                                );
+                            } else if (route.name === 'Add Deck') {
+                                return (
+                                    <Ionicons
+                                        name={
+                                            focused
+                                                ? 'add-circle'
+                                                : 'add-circle-outline'
+                                        }
+                                        size={size}
+                                        color={color}
+                                    />
+                                );
+                            }
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: '#E63946',
+                        inactiveTintColor: '#1D3557',
+                    }}
+                >
+                    <Tab.Screen name="Home" component={Home} />
+                    <Tab.Screen name="Add Deck" component={NewDeck} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        );
+    }
 }
