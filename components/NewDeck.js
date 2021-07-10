7ced1dcbf594dc46322673a4d52d18d378b0ce6e
gary-vladimir <gary@garybricks.com>
2021-07-10 14:48:07 -0500

    question text, text input and submit btn added

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
 
 function NewDeck() {
     return (
         <View style={{ padding: 20 }}>
             <Text style={styles.title}>New Deck</Text>
+            <View style={styles.card}>
+                <Text style={styles.questionTxt}>
+                    What is the title of your new deck?
+                </Text>
+                <TextInput style={styles.input} />
+                <TouchableOpacity style={styles.btn}>
+                    <Text style={{ color: '#457B9D' }}>Submit</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -14,6 +29,48 @@ const styles = StyleSheet.create({
         color: '#1D3557',
         fontWeight: 'bold',
         fontSize: 24,
+    },
+    card: {
+        marginVertical: 10,
+        backgroundColor: 'white',
+        height: 400,
+        borderRadius: 20,
+        /* box shadow */
+        shadowColor: '#adb5bd',
+        shadowOffset: { width: 0, height: 0.2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 5,
+        elevation: 5,
+        /* center content */
+        display: 'flex',
+        alignItems: 'center',
+
+        paddingTop: 50,
+    },
+    questionTxt: {
+        width: 186,
+        color: '#1D3557',
+        fontSize: 24,
+        textAlign: 'center',
+    },
+    input: {
+        textAlign: 'center',
+        backgroundColor: '#F1F1F1',
+        marginTop: 20,
+        height: 45,
+        width: 300,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+    },
+    btn: {
+        marginTop: 140,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#A8DADC',
+        height: 45,
+        width: 300,
+        borderRadius: 25,
     },
 });
 
