10c7b0a8252b8a6af52bb43483da127dfcd071e4
gary-vladimir <gary@garybricks.com>
2021-07-13 20:30:34 -0500

    added Redux middleware

--- App.js
+++ App.js
@@ -18,8 +18,9 @@ import {
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import middleware from './middleware';
 
-const store = createStore(reducer);
+const store = createStore(reducer, middleware);
 
 const Tab = createBottomTabNavigator();
 
